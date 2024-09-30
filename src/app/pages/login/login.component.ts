58bb4584592182e4fe3aefa043e22010b2806f7e
Hildialex <hildialex@hotmail.com>
2024-09-30 08:39:59 -0300

    Atualizando o repo

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+
+  public nome! : String;
+  public sobrenome!:String;
+
+  ngOnInit(): void {
+    
+    this.nome = "Hildialex";
+      
+  }
+}
